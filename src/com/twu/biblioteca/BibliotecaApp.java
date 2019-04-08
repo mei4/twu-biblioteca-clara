3df9f4389d81931512056a82ba19e57935ec5ee1
Clara Gaset <clara.g@thoughtworks.com>
2019-04-08 16:05:12 +0200

    Story 2.1 View a list of available movies

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -9,17 +9,27 @@ public class BibliotecaApp {
     public static void main(String[] args) {
 
         WelcomeMessage.sayHello();
+
         List<Book> books = new ArrayList<>(Arrays.asList(
                 new Book("To Kill a Mockingbird", "Harper Lee", 1988),
                 new Book("Pride and Prejudice", "Jane Austen", 1813),
                 new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925)));
         BooksManager booksManager = new BooksManager(books);
+
+        List<Movie> movies = new ArrayList<>(Arrays.asList(
+                new Movie("Totoro", 1988, "Hayao Miyazaki", 10),
+                new Movie("The Lion King", 1994, "Rob Minkoff and Roger Allers", 8),
+                new Movie("Captain Marvel", 2019, "Anna Boden and Ryan Fleck", 0)));
+        MoviesManager moviesManager = new MoviesManager(movies);
+
         List<String> options = new ArrayList<>(Arrays.asList(
                 "List of Books",
                 "Checkout a book",
                 "Return a book",
-                "Quit"));
-        OptionsMenu optionsMenu = new OptionsMenu(options, booksManager, null);
+                "Quit",
+                "List of movies"));
+
+        OptionsMenu optionsMenu = new OptionsMenu(options, booksManager, moviesManager);
 
         optionsMenu.showMenu();
         optionsMenu.manageOptionSelectedByTheUser();
